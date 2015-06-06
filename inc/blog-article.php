7b7cceef0af09e0d141426b67d1edf958f05ac90
andriytereschuk <shatskyat@gmail.com>
2015-06-06 10:47:28 +0300

    blog page

---
+++ inc/blog-article.php
@@ -0,0 +1,14 @@
+<section id="blog">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<h1>Блог</h1>
+			</div>
+		</div>
+
+		<div class="row">
+			<?php include_once('inc/post.php'); ?>
+			<?php include_once('inc/sidebar.php'); ?>
+		</div>
+	</div>
+</section>
