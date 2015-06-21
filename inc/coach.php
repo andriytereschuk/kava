c5c633ecee10a8dfab159019ba1c1b1e50502cdd
andriytereschuk <shatskyat@gmail.com>
2015-06-21 21:52:44 +0300

    coach page and added map to school page

---
+++ inc/coach.php
@@ -0,0 +1,83 @@
+<section id="coach" class="desc2">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<h1>тренер</h1>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-offset-2 col-md-8">
+				<div class="cafe-desc">
+					<img src="images/coach.jpg" alt="">
+					<h2>Луіджі Піранделло</h2>
+					<p>Найважливіша кавова подія року, Український Кавовий Фестиваль відбудеться у «найкавовішому» місті країни, — Львові, за підтримки кави <b>Buondi</b>. Професіонали з’їдуться з усіх куточків України, щоб виявити найкращих баріста і каптестерів. Під егідою SCAE та TM BUONDI в Україні (Асоціація Спешіалті Кави Європи) відбудуться чотири офіційні національні чемпіонати, які проводяться згідно міжнародних правил і регламенту та санкціоновані WCE (World Coffee Events). Найважливіша кавова подія року, Український Кавовий Фестиваль відбудеться у «найкавовішому» місті країни, — Львові, за підтримки кави <b>Buondi</b>. </p>
+					<p>Професіонали з’їдуться з усіх куточків України, щоб виявити найкращих баріста і каптестерів. Під егідою SCAE та TM BUONDI в Україні (Асоціація Спешіалті Кави Європи) відбудуться чотири офіційні національні чемпіонати, які проводяться згідно міжнародних правил і регламенту та санкціоновані WCE (World Coffee Events)</p>
+					<p>Найважливіша кавова подія року, Український Кавовий Фестиваль відбудеться у «найкавовішому» місті країни, — Львові, за підтримки кави Buondi. Професіонали з’їдуться з усіх куточків України, щоб виявити найкращих баріста і каптестерів. Під егідою SCAE та TM BUONDI в Україні (Асоціація Спешіалті Кави Європи) відбудуться чотири офіційні національні чемпіонати, які проводяться згідно міжнародних правил і регламенту та санкціоновані WCE (World Coffee Events). </p>
+				</div>
+
+				<div class="soc-others">
+					<a href=""><img src="images/fb-grey.png" alt=""></a>
+					<a href=""><img src="images/tw-grey.png" alt=""></a>
+					<a href=""><img src="images/g+grey.png" alt=""></a>
+					<a href=""><img src="images/mail.png" alt=""></a>
+				</div>
+			</div>
+		
+		</div>
+	</div>
+</section>
+
+<section id="sertificates">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-offset-2 col-md-8">
+				<div class="sert-wrap">
+					<h2>сертифікати</h2>
+
+					<div class="row">
+						<div class="col-sm-3 col-xs-6">
+							<a class="fancy" href="images/sert1.jpg" data-fancybox-group="gallery">
+								<img src="images/sert1.jpg" alt="">
+							</a>
+						</div>
+
+						<div class="col-sm-3 col-xs-6">
+							<a class="fancy" href="images/sert1.jpg" data-fancybox-group="gallery">
+								<img src="images/sert1.jpg" alt="">
+							</a>
+						</div>
+
+						<div class="col-sm-3 col-xs-6">
+							<a class="fancy" href="images/sert1.jpg" data-fancybox-group="gallery">
+								<img src="images/sert1.jpg" alt="">
+							</a>
+						</div>
+
+						<div class="col-sm-3 col-xs-6">
+							<a class="fancy" href="images/sert1.jpg" data-fancybox-group="gallery">
+								<img src="images/sert1.jpg" alt="">
+							</a>
+						</div>
+					</div>
+				</div>
+				
+				<a href="" class="return-left"><i class="icon-left-brown"></i>Попередній тренер</a>
+				<a href="" class="return-right">наступний тренер<i class="icon-left-brown"></i></a>
+			</div>
+		</div>
+	</div>
+
+	<script src="fancybox/jquery.fancybox.js?v=2.1.4"></script>
+	<script src="fancybox/jquery.mousewheel.js"></script>
+	<link rel="stylesheet" href="fancybox/jquery.fancybox.css?v=2.1.4" media="screen" />
+
+	<script>
+	$(function(){
+		$('.fancy').fancybox({
+			openEffect : 'elastic',
+			closeEffect : 'elastic'
+		});
+	});
+	</script>
+</section>
---
+++ inc/school/map.php
@@ -0,0 +1,71 @@
+
+
+<div id="map"></div>
+
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?v=3.20&sensor=false"></script>
+<script>
+$(function() {
+	$('#content').addClass('content-map');
+	function initialize() {
+	    var infos = [];
+	    var locations = [
+	        ['<div class="info-window"><h2>м. Львів, <br> пл. Катедральна, 6</h2><div class="i-work"><b>Пн-Сб</b><span>7:45 - 23:00</span></div><div class="i-work"><b>Нд</b><span>8:45 - 23:00</span></div></div>', 49.840362, 24.030733, 1]
+	    ];
+
+	    var map = new google.maps.Map(document.getElementById('map'), {
+	        mapTypeId: google.maps.MapTypeId.ROADMAP,
+	        zoom: 17,
+	        scrollwheel: false,
+	        zoomControl: true,
+	        zoomControlOptions: {
+	            style: google.maps.ZoomControlStyle.SMALL
+	        },
+	        center: {lat: 49.840362, lng: 24.030733},
+	        styles: [{"featureType":"administrative.province","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"landscape","elementType":"all","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","elementType":"all","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"all","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","elementType":"all","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","elementType":"all","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"transit","elementType":"geometry.fill","stylers":[{"visibility":"on"}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]}]
+	    });
+
+	    var bounds = new google.maps.LatLngBounds();
+
+	    for (i = 0; i < locations.length; i++) {
+	        var marker = new google.maps.Marker({
+	            position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+	            map: map,
+	            icon: './images/marker.png',
+	            content: locations[i][0]
+	        });
+	        var infowindow = new google.maps.InfoWindow();
+	        google.maps.event.addListener(marker, 'click', (function (marker, i, infowindow) {
+	            return function () {
+	                infowindow.setContent(this.content);
+	                infowindow.open(map, this);
+	            };
+	        })(marker, i, infowindow));
+	        bounds.extend(marker.position);
+
+	        google.maps.event.trigger(marker, 'click');
+	    }
+
+	    if (locations.length > 1)  map.fitBounds(bounds);
+
+
+	    // START INFOWINDOW CUSTOMIZE.
+	    google.maps.event.addListener(infowindow, 'domready', function() {
+
+	      var iwOuter = $('.gm-style-iw');
+	      var iwBackground = iwOuter.prev();
+
+	      iwBackground.css({'display' : 'none'});
+	      iwOuter.parent().parent().css({left: '80px',top: '45px'});
+
+	      var iwCloseBtn = iwOuter.next();
+	      iwCloseBtn.css({opacity: '1', right: '80px', top: '40px'});
+	      // The API automatically applies 0.7 opacity to the button after the mouseout event. This function reverses this event to the desired value.
+	      iwCloseBtn.mouseout(function(){
+	        $(this).css({opacity: '1'});
+	      });
+	    });
+	}
+
+	google.maps.event.addDomListener(window, 'load', initialize);
+});
+</script>
